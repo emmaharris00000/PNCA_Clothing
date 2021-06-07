02d3a863e1a5a21c9c96c6335bd28dbff02d401d
Megan McKissack <meganmckissack@gmail.com>
2021-06-07 16:33:32 -0700

    added global static assets folder and set the static file directory in settings.py

--- Clothing_Website/settings.py
+++ Clothing_Website/settings.py
@@ -126,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# Adds access to whatever static files are in the static folder for all apps in the project
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
